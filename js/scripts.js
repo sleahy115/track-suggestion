be7e8836ec964b401433b9a7f3420c19c0546547
sarah leahy <sarah leahy>
2017-01-13 16:12:28 -0700

    add form inputs and if/else statements for language choices

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#questions form").submit(function(event) {
+    var question1Input = $("input#artistic").val();
+    var question2Input = $("input#small").val();
+    var question3Input = $("input#interactive").val();
+    var question4Input = $("input#look").val();
+    var question5Input = $("input#mobile").val();
+
+    if (question1Input === "artistic" && question4Input ===look) {
+    $(".design").show();
+    }
+    else if (question2Input === "small" && question3Input === "interactive"){
+    $(".ruby").show();
+    }
+    else if (question2Input === "large" && question3Input ==="fixed" ){
+    $(".php").show();
+    }
+    event.preventDefault();
+  });
+});
