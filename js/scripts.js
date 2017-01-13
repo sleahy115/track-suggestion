040e59d9f2d8b826dd9eb7f63369e571cbe4f3e9
sarah leahy <sarah leahy>
2017-01-13 16:55:26 -0700

    add css styling, incluing text color, font and size. As well as adjust column size for form

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("#questions form").submit(function(event) {
     var question1Input = $("input#artistic").val();
     var question2Input = $("input#small").val();
@@ -6,14 +6,17 @@ $(document).ready(function(){
     var question4Input = $("input#look").val();
     var question5Input = $("input#mobile").val();
 
-    if (question1Input === "artistic" && question4Input ===look) {
+    if (question1Input === "artistic" && question4Input === "look") {
     $(".design").show();
     }
     else if (question2Input === "small" && question3Input === "interactive"){
     $(".ruby").show();
     }
-    else if (question2Input === "large" && question3Input ==="fixed" ){
+    else if (question2Input === "large" && question3Input === "fixed" ){
     $(".php").show();
+    }
+    else if (question5Input === "yes" && question2Input === "large"){
+    $(".java").show();
     }
     event.preventDefault();
   });
